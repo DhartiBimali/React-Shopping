ed72f7818f6ea7e6414d2312342522214dddd2e0
Nimesh Poudel <nimesh.pchm@lendenclub.com>
2022-05-15 08:34:45 +0545

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Productprovider from './Global/productDetails'
+import AddCartProvider from './Global/AddCart'
+import Product from './components/Product';
+import Carts from './components/Carts';
+import Notfound from './components/NotFound';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Productprovider>
+      <AddCartProvider>
+      <Router>
+      <Navbar/>
+        <Routes>
+          <Route path='/' element={<Product/>}/>
+          <Route path='/carts' element={<Carts/>}/>
+          <Route  element={<Notfound/>}/>
+
+        </Routes>
+      </Router>
+      </AddCartProvider>
+     
+    </Productprovider>
     </div>
   );
 }
